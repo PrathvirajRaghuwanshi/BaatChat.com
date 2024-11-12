059347ba796ebb01a20c0c8662c43c7faa76ffc9
PrathvirajRaghuwanshi <prathvirajraghuwanshi22@gmail.com>
2024-11-12 17:16:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+//signup button
+document.getElementById("btn2").addEventListener("click", function() {
+    window.location.href = "signup.html";
+});
+
+
+
+
+// login button
+document.getElementById("btn1").addEventListener("click", function() {
+    window.location.href = "login.html";
+});
+
+
+ //explore button
+ document.getElementById("groups").addEventListener("click", function() {
+    window.location.href = "explore.html";
+});
